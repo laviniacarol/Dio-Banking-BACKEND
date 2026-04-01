ca6218428eeaa41d72efb596569e0cec4a2ad8bf
Carol Lavinia <carolxlavinia@gmail.com>
2026-03-31 21:46:21 -0300

    user repository test done

---
+++ src/repositories/UserRepository.spec.ts
@@ -0,0 +1,28 @@
+import { EntityManager } from "typeorm"
+import { getMockEntityManager } from "../__mocks__/mockEntityManager.mock"
+import { User } from "../entities/User"
+import { UserRepository } from "./UserRepository"
+
+
+describe('UserRepository', () => {
+    let userRepository: UserRepository
+    let managerMock: Partial<EntityManager>
+
+    const mockUser: User = {
+        user_id: '12345',
+        name: 'John Doe',
+        email: 'test@exemplo.com',
+        password: 'password'
+    }
+
+    beforeAll(async () => {
+        managerMock = await getMockEntityManager({})
+        userRepository = new UserRepository(managerMock as EntityManager)
+    })
+
+    it('Deve cadastrar um novo usuário no banco de dados', async() => {
+       await userRepository.createUser(mockUser)
+
+       expect(managerMock.save).toHaveBeenCalled()
+    })
+})
--- src/repositories/UserRepository.ts
+++ src/repositories/UserRepository.ts
@@ -1,12 +1,11 @@
 import { EntityManager } from "typeorm";
-import { AppDataSource } from "../database";
 import { User } from "../entities/User";
 
 export class UserRepository {
     private manager: EntityManager
 
     constructor(
-        manager = AppDataSource.manager
+        manager: EntityManager
     ) {
         this.manager = manager;
     }
